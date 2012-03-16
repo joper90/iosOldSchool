35093d8e812cbd396062eec658433a25fc8d2906
Joe90 <joper90@gmail.com>
2012-03-16 14:02:22 +0000

    Firing now works.. !

---
+++ src/blastGame/blastGame/BangAction.h
@@ -0,0 +1,13 @@
+//
+//  BangAction.h
+//  blastGame
+//
+//  Created by AppleUser on 16/03/2012.
+//  Copyright (c) 2012 funkvoodoo.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BangAction : NSObject
+
+@end
---
+++ src/blastGame/blastGame/BangAction.m
@@ -0,0 +1,13 @@
+//
+//  BangAction.m
+//  blastGame
+//
+//  Created by AppleUser on 16/03/2012.
+//  Copyright (c) 2012 funkvoodoo.com. All rights reserved.
+//
+
+#import "BangAction.h"
+
+@implementation BangAction
+
+@end
